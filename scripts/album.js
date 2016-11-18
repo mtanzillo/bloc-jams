60d231b260bd726a424cb87027433ebc94803396
Monyett <monyett.tanzillo@gmail.com>
2016-11-18 12:32:31 -0500

    Complete assignment 27 play pause part 2

--- scripts/album.js
+++ scripts/album.js
@@ -62,15 +62,21 @@ var setCurrentAlbum = function(album) {
     }  
 };
 
-
 var findParentByClassName = function(element, targetClass) {
     if (element) {
         var currentParent = element.parentElement;
-        while (currentParent.className != targetClass && currentParent.className !== null) {
-            currentParent = currentParent.parentElement;
+        if (currentParent) {
+            while (currentParent !== null  && currentParent.className !== targetClass) {
+                currentParent = currentParent.parentElement; 
+            }         
+        } else {
+            alert('No parent found');    
         }
-        return currentParent;
+    }  
+    if (currentParent === null) {
+        alert('No parent found with class name.')
     }
+    return currentParent;
 };
 
 var getSongItem = function(element){
